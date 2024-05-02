05931d9b3c9a361699c61a1880a97dc4e1c038f5
Berkay Tuncel <berkaytncl98@gmail.com>
2024-05-02 12:55:40 +0300

    Initial Commit

---
+++ SPMsAndSDKs/SPMsAndSDKsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SPMsAndSDKsApp.swift
+//  SPMsAndSDKs
+//
+//  Created by Berkay Tuncel on 2.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SPMsAndSDKsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
